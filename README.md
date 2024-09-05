4f80e854c508d8076d67f2dbc07e8b3c33c07800
DarylAdrien <Edaryl2705@gmail.com>
2024-09-05 22:03:55 +0530

    ADDED THE REPORT-AWS-USAGE

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 
 1. REPORTING THE USAGE OF AWS RESOURCES 
+
+Added the Report-AWS-Usage file which reports the basic instances like EC2,S3,IAM,Lambda functions
+To run the file periodically ,Use the Crontab to automate it.
+The commands are given inside the file.
---
+++ report-aws-usage.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+##############################################
+Author : Daryl Adrien E
+
+Date : 05-09-2024
+
+Version : V1
+
+This script will report the AWS resource Usage
+
+###############################################
+
+
+AWS S3
+AWS EC2
+AWS Lambda
+AWS IAM
+
+
+#debug mode
+set -x    
+
+#list S3 Buckets
+echo "Print list of S3 buckets"
+aws s3 ls
+
+#list EC2 Instances
+echo "Print the EC2 instances"
+aws ec2 describe-instances
+#gives only the instanceId
+aws ec2 decribe-instances | jq '.Reservations[].Instances[].InstanceId'
+
+#list lambda
+echo "Print lambda functions"
+aws lambda list-functions
+
+#list IAM users
+echo "Print the IAM users"
+aws iam list-users
+
+
+##############################################
+#To integrate the file with crontab to run this file automatically,
+
+#crontab -e
+
+#* * * * * /home/user/sample.sh
+
+#Runs for every minute
+
+##############################################
